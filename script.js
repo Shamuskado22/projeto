59f1cc06b77f38144a6eca0ff872b479ff9a9a5f
Samuel Almeida <samucaoliveira1995@gmail.com>
2024-10-05 11:09:05 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toggle() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  const img = document.querySelector('#profile img')
+
+  if (html.classList.contains('light')) {
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('alt', 'Imagem para a versão light')
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Imagem para a versão dark")
+  }
+}
