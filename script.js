28305c3580587b013b31eb4b2a557735bca2b8b4
Samuel Almeida <samucaoliveira1995@gmail.com>
2024-10-10 11:51:36 -0300

    Site atualizado

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+//função para mudar para light e dark
 function toggle() {
+  //variável que recebe o elemento html
   const html = document.documentElement
-  html.classList.toggle('light')
+  //quando clicar no botão o elemento html recebe a class light
+  html.classList.toggle("light")
 
-  const img = document.querySelector('#profile img')
+  //variável que recebe o seletor da imagem do avatar
+  const img = document.querySelector("#profile img")
 
-  if (html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute('alt', 'Imagem para a versão light')
-  } else {
-    img.setAttribute("src", "./assets/avatar.png")
-    img.setAttribute("alt", "Imagem para a versão dark")
-  }
+  //condicional para mudar entre as imagens do avatar de light para dark e vice-versa
+  // if (html.classList.contains("light")) {
+  //   img.setAttribute("src", "./assets/avatar-light.png")
+  //   img.setAttribute("alt", "Imagem para a versão light")
+  // } else {
+  //   img.setAttribute("src", "./assets/avatar.png")
+  //   img.setAttribute("alt", "Imagem para a versão dark")
+  // }
 }
